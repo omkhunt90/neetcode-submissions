925d1c17539544b1ba903b5ebfbefcee5202d387
om khunt <omkhunt90@gmail.com>
2026-05-28 15:44:25 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fun(self, n: int) -> int:
+        add = 0
+        while n > 0:
+            d = n%10
+            n = n//10
+            add = add + d*d
+        return add
+    def isHappy(self, n: int) -> bool:
+        slow = n; fast = n
+        while fast != 1:
+            slow = self.fun(slow)
+            fast = self.fun(fast)
+            fast = self.fun(fast)
+            if slow == fast and slow != 1:
+                return False
+        return True
